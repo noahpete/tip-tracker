78b6ad78b9cd23def64c34ae4baee0e86dc92857
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-12-05 23:27:06 -0500

    Frontend init commit--basic home layout

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className="bg-[var(--background)]">
+			<div className="p-8">
+				<h1 className="p-8 text-4xl font-bold text-center">
+					Keep track of your tips--monitor your income.
+				</h1>
+				<h2 className="text-center">
+					Simplify the process of tracking tips and easily calculate your hourly wage and other
+					income statistics.
+				</h2>
+				<div className="py-12 flex justify-center items-center">
+					<Button className="">Sign up for free</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Menu } from "lucide-react";
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+
+type Props = {};
+
+export const Navbar = (props: Props) => {
+	return (
+		<div className="w-full border px-6 py-4 flex">
+			<h1 className="font-bold text-2xl">tipper</h1>
+			<div className="ml-auto -mr-2">
+				<Sheet>
+					<SheetTrigger asChild>
+						<Button variant="ghost">
+							<Menu style={{ width: "24px", height: "24px" }} />
+						</Button>
+					</SheetTrigger>
+					<SheetContent>
+						<p>Home</p>
+						<p>Tips</p>
+						<p>Analytics</p>
+					</SheetContent>
+				</Sheet>
+			</div>
+		</div>
+	);
+};
