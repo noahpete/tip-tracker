45b092a86314652efa3995481fcffcbe0d7ac385
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-12-06 22:23:11 -0500

    Updated navbar logo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
 		<div className="bg-[var(--background)]">
 			<div className="p-8">
 				<h1 className="p-8 text-4xl font-bold text-center">
-					Keep track of your tips--monitor your income.
+					Keep track of your tips--analyze your income.
 				</h1>
 				<h2 className="text-center">
 					Simplify the process of tracking tips and easily calculate your hourly wage and other
 					income statistics.
 				</h2>
 				<div className="py-12 flex justify-center items-center">
-					<Button className="">Sign up for free</Button>
+					<Button className="bg-[#05CF1C]">Sign up for free</Button>
 				</div>
 			</div>
 		</div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,25 @@ import { Menu } from "lucide-react";
 import React from "react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 type Props = {};
 
 export const Navbar = (props: Props) => {
 	return (
 		<div className="w-full border px-6 py-4 flex">
-			<h1 className="font-bold text-2xl">tipper</h1>
+			<div className="flex mt-1">
+				<div className="-mt-1">
+					<Image
+						src={"/tipper-icon.png"}
+						alt="logo"
+						width={32}
+						height={32}
+						className="mr-0.5 aspect-square"
+					/>
+				</div>
+				<h1 className="font-bold text-2xl text-[#05CF1C]">tipper</h1>
+			</div>
 			<div className="ml-auto -mr-2">
 				<Sheet>
 					<SheetTrigger>
