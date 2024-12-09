68d32e056d5a6580a51ad1c5a4c7b933f93e6f33
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-12-08 22:14:31 -0500

    Update to add Clerk provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Rubik } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -32,11 +34,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${rubik.className} antialiased`}>
-				<Navbar />
-				{children}
-			</body>
-		</html>
+		<ClerkProvider
+		// appearance={{
+		// 	baseTheme: dark,
+		// }}
+		>
+			<html lang="en">
+				<body className={`${rubik.className} antialiased`}>
+					<Navbar />
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
